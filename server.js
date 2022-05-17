43ae3d063918db9d666eb93971dcae795693c14d
beamwanatchaya <wanatchaya.bo.61@ubu.ac.th>
2022-05-17 20:14:43 +0700

    host300

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:3000"
+  origin: "http://localhost:3000",
+  origin: "http://128.199.139.142:3000"
 };
 
 app.use(cors(corsOptions));
